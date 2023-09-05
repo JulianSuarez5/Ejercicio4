6f0f4dbe75de8e995e7c7897b465b5604e07437c
JulianSuarez5 <Juacosuarez12@gmail.com>
2023-09-04 22:28:18 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class NumerosAscendentes
+{
+    static void Main()
+    {
+        int i;
+
+        for (i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.ReadKey();
+    }
+}
